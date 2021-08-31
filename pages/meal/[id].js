22f1b0e42b5e6b8eb072724e681406a81c9903ac
Lebogang Mawaya <mawayalebo@gmail.com>
2021-08-31 14:06:08 +0200

    meal content

--- pages/meal/[id].js
+++ pages/meal/[id].js
@@ -1,49 +1,63 @@
 import { useRouter } from "next/router";
 import { data } from "../../mockdata/meals";
-
+import Image from 'next/image';
 const Meal = ({meal}) => {
 
 
     return ( 
         <div className="p-10 grid grid-cols-1 md:grid-cols-6">
-            <div>
-                {meal.id}
-                {meal.name}
+            <div className="flex-0.5 relative">
+                <Image src={meal.image } layout={"fill"} />
             </div>
             <div>
-                content
+                <div className="flex 2xl">
+                    <span>{meal.name}</span>
+                </div>
+                <div className="flex">
+                    <span>{meal.price}</span>
+                </div>
+                <div>
+                    <span>R{meal.price}</span>
+                    <div>
+                        <span>{meal.category}</span>
+                    </div>
+                    <div>
+                        <span>{meal.cookTime}Min</span>
+                    </div>
+                    <div>
+                        {
+                            meal.includes && 
+                            meal.includes.map((item, index) => {
+                                return <span key={index}>{item}</span>
+                            }
+                            )
+                        }
+                    </div>
+                </div>
+                <div></div>
             </div>
         </div>
      );
 }
 
 export async function getStaticPaths() {
-    // Call an external API endpoint to get posts
-    
-    
-    // Get the paths we want to pre-render based on posts
+   
     const paths = data.map((meal) => {
         return {
             params: { id: meal.id.toString() },
         };
     });
 
-    console.log(paths);
   
-    // We'll pre-render only these paths at build time.
-    // { fallback: false } means other routes should 404.
     return { paths, fallback: false }
   }
-  // This also gets called at build time
+
 export async function getStaticProps({ params }) {
-    // params contains the post `id`.
-    // If the route is like /posts/1, then params.id is 1
-    
-    const meal = data.filter(meal => meal.id === parseInt(params.id))[0];
+
 
     
+    const meal = data.filter(meal => meal.id === parseInt(params.id))[0];
   
-    // Pass post data to the page via props
     return { props: { meal } }
 }
 
